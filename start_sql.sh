0e843c476ad1830e2e24e60ee9dbd37e4733cc58
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-04-05 14:12:51 +0800

    Update start_sql.sh

--- start_sql.sh
+++ start_sql.sh
@@ -28,7 +28,5 @@ done < ".env"
 URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Run the scripts to insert data.
-psql ${URI} -f sql/dropeverthing.sql
-psql ${URI} -f sql/DropTablesTriggers.sql
 psql ${URI} -f sql/DeleteTableContents.sql
 
