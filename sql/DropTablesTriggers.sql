d0a917f4e996f05e853edbd7c09613984bb0dbe7
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-04-05 14:23:53 +0800

    Update DropTablesTriggers.sql

--- sql/DropTablesTriggers.sql
+++ sql/DropTablesTriggers.sql
@@ -2,26 +2,28 @@ DROP TRIGGER IF EXISTS check_review ON review;
 DROP TRIGGER IF EXISTS check_capacity ON joins;
 
 
-DROP PROCEDURE create_new_activity(u_email VARCHAR,
+DROP PROCEDURE IF EXISTS create_new_activity(u_email VARCHAR,
 									u_activity_name VARCHAR,
 									u_category VARCHAR,
 									u_start_date_time TIMESTAMP,
 									u_venue VARCHAR,
 									u_capacity INTEGER);
-DROP FUNCTION check_review_func();
-DROP FUNCTION check_capacity_func();
-DROP PROCEDURE add_new_member(full_name VARCHAR,username VARCHAR,
+DROP FUNCTION IF EXISTS check_review_func();
+DROP FUNCTION IF EXISTS check_capacity_func();
+DROP PROCEDURE IF EXISTS add_new_member(full_name VARCHAR,username VARCHAR,
 							  email VARCHAR,phone_number VARCHAR,
 							  password VARCHAR);
 
 
 
-DROP TABLE joins IF EXISTS;
-DROP TABLE report IF EXISTS;
-DROP TABLE review IF EXISTS;
-DROP TABLE report IF EXISTS;
-DROP TABLE activity IF EXISTS;
-DROP TABLE category IF EXISTS;
-DROP TABLE administrator IF EXISTS;
-DROP TABLE member IF EXISTS;
-DROP TABLE users IF EXISTS;
+DROP TABLE IF EXISTS joins;
+DROP TABLE IF EXISTS report;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS administrator;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS random_comments;
+DROP TABLE IF EXISTS random_report;
+DROP TABLE IF EXISTS requests;
