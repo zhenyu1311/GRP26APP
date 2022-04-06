3351712e78481fdccd59aabc379ac57105254c51
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-04-06 22:09:22 +0800

    Update Review.sql

--- sql/Review.sql
+++ sql/Review.sql
@@ -1,9 +1,9 @@
-INSERT INTO review (activity_id,timestamp,participant,rating,comment)
-SELECT DISTINCT j.activity_id, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j.participant, ROUND(RANDOM()*(5-4)+4) AS random_rating, rc.comment
+INSERT INTO review (activity_id,timestamp,passenger,rating,comment)
+SELECT DISTINCT j.activity_id, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j.passenger, ROUND(RANDOM()*(5-4)+4) AS random_rating, rc.comment
 FROM joins j, activity a, random_comments rc
 WHERE j.activity_id = a.activity_id
 AND a.start_date_time < NOW()
-AND j.participant <> a.inviter
+AND j.passenger <> a.driver
 AND j.activity_id IN (
 	SELECT j1.activity_id
 	FROM joins j1
@@ -13,11 +13,11 @@ AND j.activity_id IN (
 LIMIT (
 	SELECT COUNT(*)
 	FROM (
-		SELECT j2.activity_id, a2.start_date_time, j2.participant
+		SELECT j2.activity_id, a2.start_date_time, j2.passenger
 		FROM joins j2, activity a2
 		WHERE j2.activity_id = a2.activity_id
 		AND a2.start_date_time < NOW()
-		AND j2.participant <> a2.inviter
+		AND j2.passenger <> a2.driver
 		AND j2.activity_id IN (
 			SELECT j3.activity_id
 			FROM joins j3
