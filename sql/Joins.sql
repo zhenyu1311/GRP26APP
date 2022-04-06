aa1478c494a06791ba29dbd71dee7784df892404
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-04-06 22:24:36 +0800

    Update Joins.sql

--- sql/Joins.sql
+++ sql/Joins.sql
@@ -8,7 +8,7 @@ INSERT INTO joins (activity_id, passenger)
 	(
 		SELECT a.activity_id, u.email
  		FROM activity a, users u
-		WHERE u.email <> a.inviter
+		WHERE u.email <> a.driver
 		ORDER BY RANDOM()
 		LIMIT (SELECT COUNT(*) FROM activity)
 	)
