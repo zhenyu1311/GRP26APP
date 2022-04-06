87cf934210bd6c0bb3e004769193acf5903cff2f
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-04-06 22:20:02 +0800

    Update Report.sql

--- sql/Report.sql
+++ sql/Report.sql
@@ -1,30 +1,30 @@
 INSERT INTO report (submitter, timestamp, report_user, comment, severity)
 (
-	SELECT j1.participant, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.participant, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'low'
+	SELECT j1.passenger, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.passenger, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'low'
 	FROM joins j1, joins j2, activity a
 	WHERE j1.activity_id = a.activity_id
 	AND j2.activity_id = a.activity_id
-	AND j1.participant <> j2.participant
+	AND j1.passenger <> j2.passenger
 	ORDER BY RANDOM()
 	LIMIT 1
 )
 UNION
 (
-	SELECT j1.participant, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.participant, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'medium'
+	SELECT j1.passenger, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.passenger, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'medium'
 	FROM joins j1, joins j2, activity a
 	WHERE j1.activity_id = a.activity_id
 	AND j2.activity_id = a.activity_id
-	AND j1.participant <> j2.participant
+	AND j1.passenger <> j2.passenger
 	ORDER BY RANDOM()
 	LIMIT 1
 )
 UNION
 (
-	SELECT j1.participant, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.participant, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'high'
+	SELECT j1.passenger, (a.start_date_time + '8 hours' + RANDOM() * (NOW()-a.start_date_time)) AS random_timestamp, j2.passenger, (SELECT comment FROM random_report ORDER BY RANDOM() LIMIT 1) AS comment, 'high'
 	FROM joins j1, joins j2, activity a
 	WHERE j1.activity_id = a.activity_id
 	AND j2.activity_id = a.activity_id
-	AND j1.participant <> j2.participant
+	AND j1.passenger <> j2.passenger
 	ORDER BY RANDOM()
 	LIMIT 1
 ); 
