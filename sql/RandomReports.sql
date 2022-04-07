16ad97b54a635280e261102460bd019b62122c5b
Phosphoraptor97 <65775637+Phosphoraptor97@users.noreply.github.com>
2022-04-08 00:09:51 +0800

    Update RandomReports.sql

--- sql/RandomReports.sql
+++ sql/RandomReports.sql
@@ -1,4 +1,4 @@
-INSERT INTO random_report VALUES ('This user spammed in the group chat');
-INSERT INTO random_report VALUES ('This user promoted illegal activities after the event');
-INSERT INTO random_report VALUES ('This user gives inappropriate comment');
+INSERT INTO random_report VALUES ('This user made too much noise');
+INSERT INTO random_report VALUES ('This user drove dangerously');
+INSERT INTO random_report VALUES ('This user had road rage');
 INSERT INTO random_report VALUES ('This user made me uncomfortable'); 
